31e21cc357c364d53919e36f5255f01471f39124
adeelxd21 <newadeel126304@gmail.com>
2026-01-14 17:10:22 +0500

    Add files via upload

---
+++ untitled.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'ADEEL MD',
+  author: '‎',
+  botName: "ADEEL MD",
+  botOwner: 'ADEEL', // Your name
+  ownerNumber: '923377555716', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.5",
+  updateZipUrl: "https://github.com/adeelxd21/Adeel--MD",, //SET YOUR REPO LINK
+};
+
+module.exports = settings;
